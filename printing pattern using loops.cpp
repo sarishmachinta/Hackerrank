ee4efefabda6dc7836e0c3687d493a7c82538eba
sarishmachinta <sarishmachintha@gmail.com>
2025-12-09 19:35:40 +0530

    Add files via upload

---
+++ printing pattern using loops.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int size = 2 * n - 1;
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            int top = i;
+            int left = j;
+            int bottom = size - 1 - i;
+            int right = size - 1 - j;
+
+            int min = top;
+            if (left < min) min = left;
+            if (bottom < min) min = bottom;
+            if (right < min) min = right;
+
+            int value = n - min;
+            printf("%d", value);
+
+            if (j < size - 1) {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
